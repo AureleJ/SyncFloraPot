0679cebe88ffadedbb1760a72236cd594b72cd70
Aurèle <aurele.joblet@gmail.com>
2026-04-29 00:59:47 +0200

    Optimise

--- lib/qrcode/qrcode_generator.c
+++ lib/qrcode/qrcode_generator.c
@@ -338,127 +338,6 @@ static void drawCodewords(BitBucket *modules, BitBucket *isFunction, BitBucket *
     }
 }
 
-#define PENALTY_N1 3
-#define PENALTY_N2 3
-#define PENALTY_N3 40
-#define PENALTY_N4 10
-
-static uint32_t getPenaltyScore(BitBucket *modules)
-{
-    uint32_t result = 0;
-
-    uint8_t size = modules->bitOffsetOrWidth;
-
-    // Adjacent modules in row having same color
-    for (uint8_t y = 0; y < size; y++)
-    {
-
-        bool colorX = bb_getBit(modules, 0, y);
-        for (uint8_t x = 1, runX = 1; x < size; x++)
-        {
-            bool cx = bb_getBit(modules, x, y);
-            if (cx != colorX)
-            {
-                colorX = cx;
-                runX = 1;
-            }
-            else
-            {
-                runX++;
-                if (runX == 5)
-                {
-                    result += PENALTY_N1;
-                }
-                else if (runX > 5)
-                {
-                    result++;
-                }
-            }
-        }
-    }
-
-    // Adjacent modules in column having same color
-    for (uint8_t x = 0; x < size; x++)
-    {
-        bool colorY = bb_getBit(modules, x, 0);
-        for (uint8_t y = 1, runY = 1; y < size; y++)
-        {
-            bool cy = bb_getBit(modules, x, y);
-            if (cy != colorY)
-            {
-                colorY = cy;
-                runY = 1;
-            }
-            else
-            {
-                runY++;
-                if (runY == 5)
-                {
-                    result += PENALTY_N1;
-                }
-                else if (runY > 5)
-                {
-                    result++;
-                }
-            }
-        }
-    }
-
-    uint16_t black = 0;
-    for (uint8_t y = 0; y < size; y++)
-    {
-        uint16_t bitsRow = 0, bitsCol = 0;
-        for (uint8_t x = 0; x < size; x++)
-        {
-            bool color = bb_getBit(modules, x, y);
-
-            // 2*2 blocks of modules having same color
-            if (x > 0 && y > 0)
-            {
-                bool colorUL = bb_getBit(modules, x - 1, y - 1);
-                bool colorUR = bb_getBit(modules, x, y - 1);
-                bool colorL = bb_getBit(modules, x - 1, y);
-                if (color == colorUL && color == colorUR && color == colorL)
-                {
-                    result += PENALTY_N2;
-                }
-            }
-
-            // Finder-like pattern in rows and columns
-            bitsRow = ((bitsRow << 1) & 0x7FF) | color;
-            bitsCol = ((bitsCol << 1) & 0x7FF) | bb_getBit(modules, y, x);
-
-            // Needs 11 bits accumulated
-            if (x >= 10)
-            {
-                if (bitsRow == 0x05D || bitsRow == 0x5D0)
-                {
-                    result += PENALTY_N3;
-                }
-                if (bitsCol == 0x05D || bitsCol == 0x5D0)
-                {
-                    result += PENALTY_N3;
-                }
-            }
-
-            // Balance of black and white modules
-            if (color)
-            {
-                black++;
-            }
-        }
-    }
-
-    // Find smallest k such that (45-5k)% <= dark/total <= (55+5k)%
-    uint16_t total = size * size;
-    for (uint16_t k = 0; black * 20 < (9 - k) * total || black * 20 > (11 + k) * total; k++)
-    {
-        result += PENALTY_N4;
-    }
-
-    return result;
-}
-
 static uint8_t rs_multiply(uint8_t x, uint8_t y)
 {
     // Russian peasant multiplication
@@ -543,7 +422,6 @@ static void performErrorCorrection(uint8_t version, uint8_t ecc, BitBucket *data
     uint16_t moduleCount = NUM_RAW_DATA_MODULES;
 
     uint8_t blockEccLen = totalEcc / numBlocks;
-    uint8_t numShortBlocks = numBlocks - moduleCount / 8 % numBlocks;
     uint8_t shortBlockLen = moduleCount / 8 / numBlocks;
 
     uint8_t shortDataBlockLen = shortBlockLen - blockEccLen;
